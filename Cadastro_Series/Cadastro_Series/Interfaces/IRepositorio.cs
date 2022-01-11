9d52a5427eae1a2c471bf68eb0a9f3534772b7cd
Wilton Junior <wilton.jr66@gmail.com>
2022-01-10 21:39:43 -0300

    Desafio de Projeto DIO-DecolaTech 2
    Projeto de cadastro básico para por em prático conhecimentos da linguagem CSharp vista no bootcamp do DecolaTech 2 da DIO

---
+++ Cadastro_Series/Cadastro_Series/Interfaces/IRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_Series.Interfaces
+{
+    internal interface IRepositorio<T>
+    {
+
+        List<T> Lista();
+        T RetornaPorID(int id);
+        void Insere(T entidade);
+        void Exclui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+
+    }
+}
